36658a462f1e72da23a778874da7ae0a522a7b04
Renell Castro <renellc9819@gmail.com>
2019-08-18 05:34:05 -0600

    Implemented query interface for ScoresQuery

--- scores.go
+++ scores.go
@@ -1,5 +1,10 @@
 package osugo
 
+import (
+	"errors"
+	"net/url"
+)
+
 // Score is a struct that contains osu! data for a given score.
 type Score struct {
 	ScoreID         string   `json:"score_id"`
@@ -18,4 +23,52 @@ type ScoresQuery struct {
 	Mods      int
 	Type      UserType
 	Limit     int
+}
+
+func (s ScoresQuery) constructQuery(key string) (string, error) {
+	validateErr := s.validateQuery()
+	if validateErr != nil {
+		return "", validateErr
+	}
+
+	reqURL := url.Values{}
+	reqURL.Add("k", key)
+	reqURL.Add("b", s.BeatmapID)
+
+	if s.User != "" {
+		reqURL.Add("u", s.User)
+	}
+
+	if s.Mode > 0 {
+		reqURL.Add("m", string(s.Mode))
+	}
+
+	if s.Type != "" {
+		reqURL.Add("type", string(s.Type))
+	}
+
+	if s.Limit > 0 {
+		reqURL.Add("limit", string(s.Limit))
+	}
+
+	return reqURL.Encode(), nil
+}
+
+func (s ScoresQuery) validateQuery() error {
+	var err error
+
+	if s.BeatmapID == "" {
+		err = errors.New("No BeatmapID value provided")
+	}
+
+	if s.Mode > 3 {
+		err = errors.New("GameMode provided is not supported by this query")
+	}
+
+	if s.Limit < 0 || s.Limit > 100 {
+		err = errors.New("Limit value provided is invalid. Either leave the Limit field blank or " +
+			"enter a value between 1 and 100")
+	}
+
+	return err
 }
